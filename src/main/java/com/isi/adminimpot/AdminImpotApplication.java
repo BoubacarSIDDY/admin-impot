46bdbdd52f890fa1863232f1dd9a864a3768b263
Boubacar Siddy Diallo <dialloboubacarsiddy2004@gmail.com>
2023-08-14 14:22:13 +0000

    first commit

---
+++ src/main/java/com/isi/adminimpot/AdminImpotApplication.java
@@ -0,0 +1,13 @@
+package com.isi.adminimpot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminImpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminImpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isi/adminimpot/AdminImpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isi.adminimpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminImpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
