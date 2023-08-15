768a3ce12e0fc02dfb2bef422870762f762565cf
Boubacar Siddy Diallo <=>
2023-08-15 23:25:23 +0000

    view thymeleaf

---
+++ src/main/java/com/isi/adminimpot/web/DeclarantController.java
@@ -0,0 +1,23 @@
+package com.isi.adminimpot.web;
+
+import com.isi.adminimpot.dao.DeclarantDao;
+import com.isi.adminimpot.entities.Declarant;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class DeclarantController {
+    private DeclarantDao declarantDao;
+
+    @GetMapping(path = "/index")
+    public String declarants(Model model){
+        List<Declarant> declarants = declarantDao.findAll();
+        model.addAttribute("listDeclarants", declarants);
+        return "declarants";
+    }
+}
